5b3497a13eb4b6b3de3909905f072458d5dd167d
tommyedmunds <73081204+tommyedmunds@users.noreply.github.com>
2021-04-22 12:59:57 -0500

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,97 @@
+'use strict';
+
+// Scores that are being selected by ID
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+let score0El = document.querySelector('#score--0');
+let score1El = document.getElementById('score--1');
+let current0El = document.getElementById('current--0');
+let current1El = document.getElementById('current--1');
+
+let diceEl = document.querySelector('.dice');
+
+let btnNew = document.querySelector('.btn--new');
+let btnRoll = document.querySelector('.btn--roll');
+let btnHold = document.querySelector('.btn--hold');
+
+const switchPlayer = function () {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
+
+//starting conditions
+let scores, currentScore, activePlayer, playing;
+
+let init = function () {
+ scores = [0, 0];
+ currentScore = 0;
+ activePlayer = 0;
+ playing = true;
+
+    score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+
+
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+      
+};
+
+init();
+
+// Roll dice functionality
+btnRoll.addEventListener('click', function(){
+    if(playing){
+    //generate random dice roll
+    let dice = Math.trunc(Math.random() * 6 ) + 1;
+    console.log(dice);
+    //display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //check for rolled 1; if tru, then other player's turn
+    if(dice !== 1) {
+        //add dice to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+    } else {
+        switchPlayer ();
+        //switch to next player
+        
+    }
+    }
+});
+
+btnHold.addEventListener('click', function () {
+    if(playing){
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    // check if players score is >= 100
+    if (scores[activePlayer] >= 20) {
+        //finish game
+        playing = false;
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+        diceEl.classList.add('hidden');
+    } else {
+        switchPlayer();
+    }
+}
+});
+
+btnNew.addEventListener('click', init);
